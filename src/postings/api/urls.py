db23ff4e7b646f2a8bc6d772aec7d9758827616e
Artem Chernyaev <askanio234@gmail.com>
2018-01-23 00:19:56 +0300

    Implemented List, Create View for BlogPost and removed sqlite

--- src/postings/api/urls.py
+++ src/postings/api/urls.py
@@ -1,7 +1,8 @@
-from django.urls import re_path
-from .views import BlogPostRUDView
+from django.urls import re_path, path
+from .views import BlogPostRUDView, BlogPostCreateView
 
 
 urlpatterns = [
-    re_path('(?P<pk>\d+)', BlogPostRUDView.as_view(), name='post-rud')
+    re_path('(?P<pk>\d+)', BlogPostRUDView.as_view(), name='post-rud'),
+    path('', BlogPostCreateView.as_view(), name='post-create'),
 ]
